88784d6494f03ab96d4b2ca8d0b14e9035726515
nihlaeth <info@nihlaeth.nl>
2016-09-05 17:40:02 +0200

    aggregate relative axis events for speed

--- mouse.py
+++ mouse.py
@@ -1,33 +1,26 @@
 import pyautogui
+size_x, size_y = pyautogui.size()
 
-def move_mouse(axis, amount):
+def move(delta_x, delta_y):
+    """Move mouse to new position and/or exit screen."""
     pos_x, pos_y = pyautogui.position()
-    fut_x = pos_x
-    fut_y = pos_y
-    if axis == 0:
-        fut_x += amount
-    elif axis == 1:
-        fut_y += amount
-    else:
-        return "err_axis"
+    fut_x = pos_x + delta_x
+    fut_y = pos_y + delta_y
 
     if pyautogui.onScreen(fut_x, fut_y):
         pyautogui.moveTo(fut_x, fut_y, 0)
         return "ok"
     else:
         # move mouse to edge and exit screen
-        if axis == 0:
-            if fut_x < 0:
-                pyautogui.moveTo(0, fut_y, 0)
-                return "exit left"
-            else:
-                pyautogui.moveTo(pyautogui.size()[0] - 1, fut_y, 0)
-                return "exit right"
+        if fut_x < 0:
+            pyautogui.moveTo(0, fut_y, 0)
+            return "exit left"
+        elif fut_x >= size_x:
+            pyautogui.moveTo(size_x - 1, fut_y, 0)
+            return "exit right"
+        elif fut_y < 0:
+            pyautogui.moveToo(fut_x, 0, 0)
+            return "exit up"
         else:
-            if fut_y < 0:
-                pyautogui.moveToo(fut_x, 0, 0)
-                return "exit up"
-            else:
-                pyautogui.moveTo(fut_x, pyautogui.size()[1] - 1, 0)
-                return "exit down"
-
+            pyautogui.moveTo(fut_x, size_y - 1, 0)
+            return "exit down"
--- read_host.py
+++ read_host.py
@@ -1,12 +1,62 @@
 import evdev
 import asyncio
 import mouse
+from threading import Timer, Lock
+
+mouse_movement = [0, 0]
+mouse_lock = Lock()
+
+class Periodic(object):
+    """
+    A periodic task running in threading.Timers
+    """
+
+    def __init__(self, interval, function, *args, **kwargs):
+        self._lock = Lock()
+        self._timer = None
+        self.function = function
+        self.interval = interval
+        self.args = args
+        self.kwargs = kwargs
+        self._stopped = True
+        if kwargs.pop('autostart', True):
+            self.start()
+
+    def start(self, from_run=False):
+        self._lock.acquire()
+        if from_run or self._stopped:
+            self._stopped = False
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self._lock.release()
+
+    def _run(self):
+        self.start(from_run=True)
+        self.function(*self.args, **self.kwargs)
+
+    def stop(self):
+        self._lock.acquire()
+        self._stopped = True
+        self._timer.cancel()
+        self._lock.release()
+
+def move_mouse(*args, **kwargs):
+    """Pass on aggregated mouse movements."""
+    mouse_lock.acquire()
+    if any([value != 0 for value in mouse_movement]):
+        mouse.move(mouse_movement[0], mouse_movement[1])
+        # todo: check return value
+        mouse_movement[0] = 0
+        mouse_movement[1] = 0
+    mouse_lock.release()
 
 async def print_events(device):
     async for event in device.async_read_loop():
         print(device.fn, evdev.categorize(event), sep=': ')
         if event.type == evdev.ecodes.EV_REL:
-            print(mouse.move_mouse(event.code, event.value))
+            mouse_lock.acquire()
+            mouse_movement[event.code] += event.value
+            mouse_lock.release()
 
 if __name__ == "__main__":
     all_devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
@@ -23,7 +73,12 @@ if __name__ == "__main__":
             device.grab()
             print("grab")
             asyncio.ensure_future(print_events(device))
+    mouse_mover = Periodic(0.1, move_mouse)
+    mouse_mover.start()
     loop = asyncio.get_event_loop()
-    loop.run_forever()
-    for device in devices:
-        device.ungrab()
+    try:
+        loop.run_forever()
+    finally:
+        mouse_mover.stop()
+        for device in devices:
+            device.ungrab()
