e7058918338b6bee87cac28d7047748f6ee551be
nihlaeth <info@nihlaeth.nl>
2016-09-05 12:23:33 +0200

    set duration to 0

--- mouse.py
+++ mouse.py
@@ -12,22 +12,22 @@ def move_mouse(axis, amount):
         return "err_axis"
 
     if pyautogui.onScreen(fut_x, fut_y):
-        pyautogui.moveTo(fut_x, fut_y)
+        pyautogui.moveTo(fut_x, fut_y, 0)
         return "ok"
     else:
         # move mouse to edge and exit screen
         if axis == 0:
             if fut_x < 0:
-                pyautogui.moveTo(0, fut_y)
+                pyautogui.moveTo(0, fut_y, 0)
                 return "exit left"
             else:
-                pyautogui.moveTo(pyautogui.size()[0] - 1, fut_y)
+                pyautogui.moveTo(pyautogui.size()[0] - 1, fut_y, 0)
                 return "exit right"
         else:
             if fut_y < 0:
-                pyautogui.moveToo(fut_x, 0)
+                pyautogui.moveToo(fut_x, 0, 0)
                 return "exit up"
             else:
-                pyautogui.moveTo(fut_x, pyautogui.size()[1] - 1)
+                pyautogui.moveTo(fut_x, pyautogui.size()[1] - 1, 0)
                 return "exit down"
 
