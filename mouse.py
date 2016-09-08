17b5bf57cb545991c244dbdb40e4d055f0f680a2
nihlaeth <info@nihlaeth.nl>
2016-09-09 01:14:56 +0200

    mored info on screen exit and enter function

--- mouse.py
+++ mouse.py
@@ -8,6 +8,19 @@ pyautogui.FAILSAFE = False
 
 size_x, size_y = pyautogui.size()
 
+def enter(exit_direction, percentage):
+    """Enter screen in logical place."""
+    if exit_direction == "up":
+        pyautogui.moveTo((size_x - 1) / percentage, size_y - 1, 0)
+    elif exit_direction == "down":
+        pyautogui.moveTo((size_x - 1) / percentage, 0, 0)
+    elif exit_direction == "left":
+        pyautogui.moveTo(size_x - 1, (size_y - 1) / percentage, 0)
+    elif exit_direction == "right":
+        pyautogui.moveTo(0, (size_y - 1) / percentage, 0)
+    else:
+        print("unknown exit direction {}".format(exit_direction))
+
 def move(delta_x, delta_y, delta_wheel):
     """Move mouse to new position and/or exit screen."""
     pos_x, pos_y = pyautogui.position()
@@ -24,13 +37,13 @@ def move(delta_x, delta_y, delta_wheel):
         # move mouse to edge and exit screen
         if fut_x < 0:
             pyautogui.moveTo(0, fut_y, 0.01)
-            return "exit left"
+            return "exit left {}".format(fut_y / size_y)
         elif fut_x >= size_x:
             pyautogui.moveTo(size_x - 1, fut_y, 0.01)
-            return "exit right"
+            return "exit right {}".format(fut_y / size_y)
         elif fut_y < 0:
             pyautogui.moveTo(fut_x, 0, 0.01)
-            return "exit up"
+            return "exit up {}".format(fut_x / size_x)
         else:
             pyautogui.moveTo(fut_x, size_y - 1, 0.01)
-            return "exit down"
+            return "exit down {}".format(fut_x / size_x)
